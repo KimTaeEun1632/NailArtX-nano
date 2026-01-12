d8863bc35b6054d51cfcdf1f26ec1ec6c999b97a
KimTaeEun1632 <qpwoal1324@naver.com>
2026-01-12 16:25:23 +0900

    chore: ToDo 추가

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,6 +6,9 @@ import CTASection from "./components/CTASection.jsx";
 // ToDo: 랜딩페이지 컴포넌트 분리
 // 회원가입, 로그인 기능, 갤러리 기능 추가
 
+// 사용자 피드백 : 생성된 이미지가 너무 고퀄리티라는 피드백.
+// 난이도 별로 1. 초보자용, 2. 실무 네일샵용, 3. 전무가(아트, 포트폴리오용)로 구분 버튼 추가.
+
 export default function Landing() {
   return (
     <div className="light font-display bg-background-light dark:bg-background-dark text-[#151118] dark:text-white antialiased selection:bg-primary/30 selection:text-primary-dark">
