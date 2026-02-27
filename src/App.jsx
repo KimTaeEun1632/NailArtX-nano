47ef0e60d01227c1a8f20d9a103c96c77ee230c1
KimTaeEun1632 <qpwoal1324@naver.com>
2026-02-27 19:22:40 +0900

    chore: App.jsx test 경로 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Generate from "./Generate";
-import Test from "./Test";
 import Layout from "./components/Layout";
 
 export default function App() {
@@ -10,7 +9,6 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Landing />} />
         <Route path="/generate" element={<Generate />} />
-        <Route path="/test" element={<Test />} />
       </Route>
     </Routes>
   );
